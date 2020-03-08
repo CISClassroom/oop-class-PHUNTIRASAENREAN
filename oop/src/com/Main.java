36002275d060cfb36cb37ee718d8f2c1bd383f77
ภัณฑิรา แสนเรียน <58883293+PHUNTIRASAENREAN@users.noreply.github.com>
2020-03-08 23:52:03 +0700

    pop

---
+++ oop/src/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello OOP");
+        Student detail = new Student();
+        detail.GetStudent();
+    }
+}
+
---
+++ oop/src/com/Student.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Student {
+    String name = "นางสาวภัณฑิรา แสนเรียน";
+    String id = "623410035-5";
+    int age = 2563-2543;
+    public void GetStudent()
+    {
+        System.out.println("ชื่อ-สกุล : " + name);
+        System.out.println("รหัสนักศึกษา : " + id);
+        System.out.println("อายุ " + age + " ปี");
+        System.out.println("สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ");
+    }
+}
+
+
+
